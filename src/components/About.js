2502bc5520cc92f3fb279aefb2e8c564c3c7a9e0
Smit Parmar <72307098+smit6366@users.noreply.github.com>
2024-09-19 17:24:57 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
+    <p>This app is created for the purpose of learning server side scripting. Please ignore the UI. Thank You</p>
     </div>
   )
 }
