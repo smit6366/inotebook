15f214524f160b1cc63b7af4abb18ededbec154d
Smit Parmar <72307098+smit6366@users.noreply.github.com>
2024-09-21 13:39:23 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
     const [credentials, setCredentials]=useState({email:"",password:""})
     const handlesubmit= async (e)=>{
         e.preventDefault();
-        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        const response = await fetch(`https://inotebook-inky.vercel.app/api/auth/login`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
